deba36920fbbe6dab6b171fd7751015f6e7c3ed4
Jason Gayle <jasonblue1626@gmail.com>
2020-08-02 16:35:32 -0500

    Completed SNMP pattoo shared installation

---
+++ setup/_pattoo_agent_snmp/configure.py
@@ -0,0 +1,37 @@
+import os
+from pattoo_shared.installation import configure, shared
+from pattoo_shared import files
+
+
+def install(daemon_list):
+    """Start configuration process.
+    Args:
+        None
+    Returns:
+        None
+    """
+    # Initialize key variables
+    if os.environ.get('PATTOO_CONFIGDIR') is None:
+        os.environ['PATTOO_CONFIGDIR'] = '{0}etc{0}pattoo'.format(os.sep)
+    config_directory = os.environ.get('PATTOO_CONFIGDIR')
+
+    snmp_agent_dict = {
+            'polling_interval': 300,
+    }
+
+    # Attempt to create configuration directory
+    files.mkdir(config_directory)
+
+    # Create the pattoo user and group
+    configure.create_user('pattoo', '/nonexistent', ' /bin/false', True)
+
+    # Attempt to change the ownership of the configuration directory
+    shared.chown(config_directory)
+
+    for daemon in daemon_list:
+        config_file = configure.pattoo_config(
+                                            daemon,
+                                            config_directory,
+                                            snmp_agent_dict)
+
+        configure.check_config(config_file, snmp_agent_dict)
